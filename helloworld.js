cafbe958c6172c051f4a9666af4d75773e53ae0b
lnosal1 <lnosal1@live.maryville.edu>
2022-01-16 14:50:50 -0600

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-
 var http = require("http")
 
 http.createServer(function (request, response) {
@@ -10,8 +8,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Laurens World\n')
-}).listen(3333)
+   response.end('Hello World\n')
+}).listen(3000)
 
 // Console will print the message
 console.log('Server running')
